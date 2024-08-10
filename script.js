d5e8dbe9fabac2f6e2baed843cc464d5da326ce6
ArifRaihan <arifraihan062003@gmail.com>
2024-08-11 00:13:06 +0800

    Done General Game Mechanics

---
+++ script.js
@@ -0,0 +1,61 @@
+let squareTiles = document.querySelectorAll(".box");
+let playButton = document.querySelector("#play-button");
+let cashOutButton = document.querySelector("#cashOut-button");
+let riskSelect = document.querySelector("#risk");
+let betAmount = document.querySelector("#amountInput");
+
+let bombsAmount = 5;
+let isPlaying = false;
+let tilesAmounts = squareTiles.length;
+riskSelect.addEventListener("change", () => {
+    bombsAmount = riskSelect.value;
+    console.log(bombsAmount);
+});
+squareTiles.forEach((squareTile) => {
+    squareTile.addEventListener("click", () => {
+        if (isPlaying) {
+            if (!squareTile.classList.contains("clicked")) {
+                if (Math.random() < bombsAmount / tilesAmounts) {
+                    isPlaying = false;
+                    playButton.disabled = false;
+                    console.log("LOSE");
+                    squareTile.textContent = "BOMB";
+                } else {
+                    console.log("WIN");
+                    squareTile.textContent = "DIAMOND";
+                }
+                squareTile.classList.add("clicked");
+            } else {
+                console.log("ALREADY CLICKED");
+            }
+        } else {
+            console.log("Press Play Button to Start");
+        }
+    });
+});
+
+playButton.addEventListener("click", () => {
+    console.log("Play Button clicked");
+    if (betAmount.value < 0 || betAmount.value == "") {
+        console.log("Invalid Bet");
+    } else {
+        playButton.disabled = true;
+        cashOutButton.disabled = false;
+        squareTiles.forEach((squareTile) => {
+            squareTile.classList.remove("clicked");
+            squareTile.textContent = "";
+        });
+        isPlaying = true;
+    }
+});
+
+cashOutButton.addEventListener("click", () => {
+    console.log("Cashed Out");
+    isPlaying = false;
+    cashOutButton.disabled = true;
+    playButton.disabled = false;
+    squareTiles.forEach((squareTile) => {
+        squareTile.classList.remove("clicked");
+        squareTile.textContent = "";
+    });
+});
