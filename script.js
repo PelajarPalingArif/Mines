91283cd647e907a9ddd1ad755237121c7aac40c9
Magnum Opus <arifraihan062003@gmail.com>
2024-08-12 00:29:16 +0800

    Fix Revealing Mines

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ squareTiles.forEach((squareTile) => {
     squareTile.addEventListener("click", () => {
 
         if (isPlaying) {
-            console.log(`WIN CHANCE : ${Math.round((1 - bombsAmount / tilesAmounts) * 100,2)}, LOSE CHANCE : ${Math.round(100 - (1 - bombsAmount / tilesAmounts) * 100,2)} , TA : ${tilesAmounts}, BA : ${bombsAmount} `  );
+            console.log(`WIN CHANCE : ${Math.round((1 - bombsAmount / tilesAmounts) * 100, 2)}, LOSE CHANCE : ${Math.round(100 - (1 - bombsAmount / tilesAmounts) * 100, 2)} , TA : ${tilesAmounts}, BA : ${bombsAmount} `);
             squareTile.style["backgroundColor"] = "rgb(87, 87, 87)";
             if (!squareTile.classList.contains("clicked")) {
                 squareTile.classList.add("clicked");
@@ -25,26 +25,29 @@ squareTiles.forEach((squareTile) => {
                     cashOutButton.disabled = true;
                     // LOSE
                     squareTile.innerHTML = `<img src="bombedit.png" alt="" srcset="">`;
-
+                    // squareTile.style["outline"] = "3px solid red";
                     let remainingSquareTiles = document.querySelectorAll(".box:not(.clicked)");
                     let tempBombAmount = bombsAmount - 1;
                     let tempSquareTiles = remainingSquareTiles.length;
                     console.log(`Bomb Left : ${tempBombAmount}, Tiles Left : ${tempSquareTiles}`);
                     remainingSquareTiles.forEach((ele) => {
                         ele.style["background-color"] = "rgb(87, 87, 87)";
-                        if(Math.random() < tempBombAmount / Math.max(tempSquareTiles,1)){
-                            ele.innerHTML =`<img src="bombedit.png" alt="">`;
+                        if (Math.random() < tempBombAmount / Math.max(tempSquareTiles, 1)) {
+                            // ele.style["outline"] = "3px solid red";
+                            ele.innerHTML = `<img src="bombedit.png" alt="">`;
                             tempBombAmount--;
                         }
                         else {
-                            ele.innerHTML =`<img src="diamondedit.png" alt="">`;
+                            // ele.style["outline"] = "3px solid blue";
+                            ele.innerHTML = `<img src="diamondedit.png" alt="">`;
                         }
 
                         tempSquareTiles--;
-                        
+
                     })
                 } else {
                     // WIN
+                    // squareTile.style["outline"] = "3px solid blue";
                     squareTile.innerHTML = `<img src="diamondedit.png" alt="">`;
                 }
             } else {
@@ -59,7 +62,7 @@ squareTiles.forEach((squareTile) => {
 
 playButton.addEventListener("click", () => {
     tilesAmounts = squareTiles.length;
-    console.log("Play Button clicked");
+    console.log("TA : " + tilesAmounts + " BA : " + bombsAmount);
     if (betAmount.value < 0 || betAmount.value == "") {
         console.log("Invalid Bet");
     } else {
@@ -83,6 +86,7 @@ cashOutButton.addEventListener("click", () => {
         squareTile.classList.remove("clicked");
         squareTile.textContent = "";
         squareTile.style["backgroundColor"] = "grey";
+        squareTile.style["outline"] = "none";
 
     });
 });
