f505051121be68e08c8a7e080f5cd6e49cecbc61
Magnum Opus <arifraihan062003@gmail.com>
2024-08-12 11:05:16 +0800

    Added Cashout Mechanic

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ let squareTiles = document.querySelectorAll(".box");
 let playButton = document.querySelector("#play-button");
 let cashOutButton = document.querySelector("#cashOut-button");
 let riskSelect = document.querySelector("#risk");
-let betAmount = document.querySelector("#amountInput");
+let betInput = document.querySelector("#amountInput");
+let betMoney;
+let accountMoney = document.querySelector("#money");
+let moneyTrack = 0;
+let probHappening = 1;
+let cashOut = 0;
+
+let edge = 0.13;
+let winCount = 0;
 
 let bombsAmount = 10;
 let isPlaying = false;
@@ -15,7 +23,8 @@ squareTiles.forEach((squareTile) => {
     squareTile.addEventListener("click", () => {
 
         if (isPlaying) {
-            console.log(`WIN CHANCE : ${Math.round((1 - bombsAmount / tilesAmounts) * 100, 2)}, LOSE CHANCE : ${Math.round(100 - (1 - bombsAmount / tilesAmounts) * 100, 2)} , TA : ${tilesAmounts}, BA : ${bombsAmount} `);
+            let currentWinChance = (1 - bombsAmount / tilesAmounts);   
+            console.log(`WIN CHANCE : ${currentWinChance}, LOSE CHANCE : ${1 - currentWinChance} , TA : ${tilesAmounts}, BA : ${bombsAmount} `);
             squareTile.style["backgroundColor"] = "rgb(87, 87, 87)";
             if (!squareTile.classList.contains("clicked")) {
                 squareTile.classList.add("clicked");
@@ -23,6 +32,7 @@ squareTiles.forEach((squareTile) => {
                     isPlaying = false;
                     playButton.disabled = false;
                     cashOutButton.disabled = true;
+                    cashOutButton.textContent = "Cash Out";
                     // LOSE
                     squareTile.innerHTML = `<img src="bombedit.png" alt="" srcset="">`;
                     // squareTile.style["outline"] = "3px solid red"; 
@@ -49,6 +59,15 @@ squareTiles.forEach((squareTile) => {
                     // WIN
                     // squareTile.style["outline"] = "3px solid blue";
                     squareTile.innerHTML = `<img src="diamondedit.png" alt="">`;
+                    
+                    winCount++;
+
+                    probHappening = probHappening * currentWinChance;
+                    cashOut = (1 / probHappening) - (edge * winCount);
+                    console.log("Probability Happening : " + probHappening);
+                    console.log("Cashout : " + cashOut);
+                    cashOutButton.textContent = (cashOut * betMoney).toFixed(2);
+                    
                 }
             } else {
                 console.log("ALREADY CLICKED");
@@ -62,10 +81,17 @@ squareTiles.forEach((squareTile) => {
 
 playButton.addEventListener("click", () => {
     tilesAmounts = squareTiles.length;
+    probHappening = 1;
+    winCount = 0;
+    cashOut = 0;
     console.log("TA : " + tilesAmounts + " BA : " + bombsAmount);
-    if (betAmount.value < 0 || betAmount.value == "") {
+    if (betInput.value < 0 || betInput.value == "") {
         console.log("Invalid Bet");
     } else {
+        betMoney = betInput.value;
+        console.log("AM : " + parseFloat(accountMoney.textContent) + " BM : " + parseFloat(betMoney));    
+        moneyTrack = (parseFloat(accountMoney.textContent) - parseFloat(betMoney)).toFixed(2);
+        accountMoney.textContent = moneyTrack;
         playButton.disabled = true;
         cashOutButton.disabled = false;
         squareTiles.forEach((squareTile) => {
@@ -78,10 +104,15 @@ playButton.addEventListener("click", () => {
 });
 
 cashOutButton.addEventListener("click", () => {
-    console.log("Cashed Out");
+    
+    
+    moneyTrack = parseFloat(moneyTrack) + (cashOut * betMoney);
+    console.log("Money Track 1 : " + moneyTrack);
+    accountMoney.textContent = moneyTrack.toFixed(2);
     isPlaying = false;
     cashOutButton.disabled = true;
     playButton.disabled = false;
+    cashOutButton.textContent = "Cash Out";
     squareTiles.forEach((squareTile) => {
         squareTile.classList.remove("clicked");
         squareTile.textContent = "";
